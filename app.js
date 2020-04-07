f8aaec9863d9778643d6d6e7057d43f499861b95
myhkas <mmyhkas@gmail.com>
2020-04-06 21:56:37 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var name = "Myhkas Nallas";
+var isTired = false;
+var age = 24;
+var initial = "M";
+
+if (name === Bob){
+    console.log("You are a registered guest");
+} else if (name === Myhkas Nallas){
+    console.log("Welcome Admin!");
+} else {
+    console.log("Security Breach!");
+}
+
+// console.log() = displays
+console.log(name + age + isTired);
+// "=" - assignment, "==" - comparative, "===" - comparative
+if (isTired === true) {
+  console.log("I am tired!");
+} else {
+  console.log("second wind, I love javascript");
+}
+
+var numberInt = 5;
+var numberString = "5";
+
+// double equal looks at the value, not the datatype
+if (numberInt === numberString) {
+  console.log("These are the same");
+} else {
+  console.log("These are not the same");
+}
+
+var arr1 = ["Myhkas", "Echague", "Nallas"];
+console.log(arr1[1]);
---
+++ array.js
@@ -0,0 +1,7 @@
+var names = ["Myhkas", "Echague", "Nallas"];
+var numbers = [1, 2, 3, 4, 5];
+var mix = [true, false, 1, 2, names];
+
+console.log(names);
+console.log(names[0]);
+console.log(mix);
